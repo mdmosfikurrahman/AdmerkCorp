8e5198eb13cc61464e6242bcba921f4237eb6a67
Mosfik <mdmosfikurrahman.cse@gmail.com>
2024-03-04 00:26:55 +0600

    Initial commit

---
+++ src/main/java/com/AdmerkCorp/AdmerkCorpApplication.java
@@ -0,0 +1,13 @@
+package com.AdmerkCorp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmerkCorpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmerkCorpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AdmerkCorp/AdmerkCorpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AdmerkCorp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmerkCorpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

b59e9c3ddc966e26f4ef9b925ee345559e5e6cfa
Mosfik <mdmosfikurrahman.cse@gmail.com>
2024-03-04 10:57:00 +0600

    API Doc and Terms & Condition Page Added

---
+++ src/main/java/com/AdmerkCorp/controller/AdmerkCorpController.java
@@ -0,0 +1,18 @@
+package com.AdmerkCorp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdmerkCorpController {
+
+    @GetMapping("/")
+    public String getAPIDoc() {
+        return "apiDoc";
+    }
+
+    @GetMapping("/terms")
+    public String getTermsAndConditions() {
+        return "terms";
+    }
+}
